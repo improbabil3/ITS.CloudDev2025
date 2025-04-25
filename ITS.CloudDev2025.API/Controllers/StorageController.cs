f1035dc8618a54732821de32669480f0d900c88b
Alessandro Macrì <alessandro.macri@reti.it>
2025-04-25 17:37:23 +0200

    Add storage account management

---
+++ ITS.CloudDev2025.API/Controllers/StorageController.cs
@@ -0,0 +1,121 @@
+﻿using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+using ITS.CloudDev2025.API.DTOs;
+using ITS.CloudDev2025.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITS.CloudDev2025.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly string _blobStorageConnectionString;
+        private readonly string _customerTableName;
+
+        public StorageController(ILogger<StorageController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _blobStorageConnectionString = configuration["BlobStorage:ConnectionString"];
+            _customerTableName = configuration["BlobStorage:CustomerTableName"];
+
+        }
+
+        /// <summary>
+        /// Get a blob file from the specified container in Azure Blob Storage.
+        /// </summary>
+        /// <param name="containerName">The container name.</param>
+        /// <param name="blobName">The blob file name.</param>
+        /// <returns>Blob sas uri.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        [HttpGet("blob/{containerName}/{blobName}")]
+        [ProducesResponseType<Uri>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetBlobByName(string containerName, string blobName)
+        {
+            // Create a BlobServiceClient
+            var blobServiceClient = new BlobServiceClient(_blobStorageConnectionString);
+
+            // Get the container client
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Check if container exists
+            if (!containerClient.Exists())
+                throw new InvalidOperationException($"Container '{containerName}' does not exist.");
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            Uri sasUri = blobClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Write, DateTimeOffset.UtcNow.AddMinutes(10));
+
+            return Ok(sasUri);
+        }
+
+        /// <summary>
+        /// Upload a blob to the specified container in Azure Blob Storage.
+        /// </summary>
+        /// <param name="file">The file from httprequest.</param>
+        /// <param name="containerName">The container name.</param>
+        /// <returns>Blob created</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        [HttpPost("upload")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UploadBlob([FromBody] IFormFile file, [FromQuery] string containerName)
+        {
+            if (string.IsNullOrWhiteSpace(containerName))
+                throw new InvalidOperationException("Container name is invalid.");
+
+            if (file == null || file.Length == 0)
+                throw new InvalidOperationException("File is invalid.");
+
+            // Create a BlobServiceClient
+            var blobServiceClient = new BlobServiceClient(_blobStorageConnectionString);
+
+            // Get the container client
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Check if container exists
+            if (!containerClient.Exists())
+                throw new InvalidOperationException($"Container '{containerName}' does not exist.");
+
+            var blobClient = containerClient.GetBlobClient(file.FileName);
+            using (var stream = file.OpenReadStream())
+            {
+                blobClient.Upload(stream);
+            }
+
+            return Ok(true);
+        }
+
+        /// <summary>
+        /// Save a customer to the Table Storage.
+        /// </summary>
+        /// <param name="name">The user name.</param>
+        /// <param name="surname">The user surname.</param>
+        /// <param name="email">The user email.</param>
+        /// <param name="phoneNumber">The user phone number.</param>
+        /// <returns>Customer id created.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        [HttpPost("customer/save")]
+        [ProducesResponseType<string>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> SaveCustomer(string name, string surname, string email, string phoneNumber)
+        {
+            // Create a new TableClient
+            var tableClient = new TableClient(_blobStorageConnectionString, _customerTableName);
+
+            if (tableClient == null)
+                throw new InvalidOperationException($"Table '{_customerTableName}' does not exist.");
+
+            // Create a new customer entity
+            Customer customer = Customer.Create(name, surname, email, phoneNumber);
+
+            // Insert the entity into the table
+            await tableClient.AddEntityAsync(customer);
+
+            return Ok(customer.RowKey);
+        }
+    }
+}
---
+++ ITS.CloudDev2025.API/Entities/Customer.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Data.Tables;
+using Azure;
+
+namespace ITS.CloudDev2025.API.Entities
+{
+    public class Customer : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        /// <summary>
+        /// Constructor for the Customer class.
+        /// </summary>
+        /// <param name="firstName">Customer name.</param>
+        /// <param name="lastName">Customer surname.</param>
+        /// <param name="email">Customer email.</param>
+        /// <param name="phoneNumber">Customer phone number.</param>
+        protected Customer(string firstName, string lastName, string email, string phoneNumber)
+        {
+            PartitionKey = CalculatePartitionKey(firstName, lastName);
+            RowKey = $"{Guid.NewGuid()}";
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the Customer class.
+        /// </summary>
+        /// <param name="firstName">Customer name.</param>
+        /// <param name="lastName">Customer surname.</param>
+        /// <param name="email">Customer email.</param>
+        /// <param name="phoneNumber">Customer phone number.</param>
+        /// <returns></returns>
+        public static Customer Create(string firstName, string lastName, string email, string phoneNumber)
+        {
+            return new Customer(firstName, lastName, email, phoneNumber);
+        }
+
+        /// <summary>
+        /// Calculates the partition key based on the first name and last name.
+        /// </summary>
+        /// <param name="firstName">The customer first name.</param>
+        /// <param name="lastName">The customer last name.</param>
+        /// <returns></returns>
+        private static string CalculatePartitionKey(string firstName, string lastName)
+        {
+            // Example logic to generate a partition key based on firstName and lastName  
+            return $"{lastName?.ToUpperInvariant()}_{firstName?.ToUpperInvariant()}";
+        }
+    }
+}
